82808fb97687b0ff845dda48a33c70155969904d
shengyuzeng <1091969093@qq.com>
2020-11-30 10:50:44 +0800

    master init

---
+++ src/components/common/ShMarquee.vue
@@ -0,0 +1,18 @@
+<!--
+  跑马灯组件
+-->
+<template>
+  <div class="sh-marquee-wrap" ref="sh-marquee-wrap">
+    <div>syzMarqueeTest</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShMarquee"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -2,8 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import util from "@/utils/util";
 
+Vue.prototype.$axios = axios
+Vue.use(ElementUI);
 Vue.config.productionTip = false
+Vue.prototype.$utils = util
 
 new Vue({
   router,
